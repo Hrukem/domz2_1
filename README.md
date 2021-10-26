c641224ce52907ad55aa0b060fda8f016c60918d
Hrukem <junghe@yandex.ru>
2021-10-26 08:52:55 +0300

    change add check lifetime data, change README.md

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # domz2_1
+
+    package main
+
+    import (
+    "fmt"
+    "github.com/Hrukem/domz2_1"
+    "time"
+    )
+
+    func main() {
+    c := domz2_1.New()
+
+	fmt.Println("\nset three value with lifetime 5 second")
+	c.Set("1", 12345, 5)
+	c.Set("2", 54321, 5)
+	c.Set("3", "Golang", 5)
+	fmt.Println(c.Get("1"))
+	fmt.Println(c.Get("2"))
+	fmt.Println(c.Get("3"))
+
+	time.Sleep(time.Second * 2)
+	fmt.Println("\nfrom 3 second")
+	fmt.Println(c.Get("1"))
+	fmt.Println(c.Get("2"))
+	fmt.Println(c.Get("3"))
+
+	fmt.Println("\ndelete key \"2\"")
+	c.Delete("2")
+	fmt.Println(c.Get("1"))
+	fmt.Println(c.Get("2"))
+	fmt.Println(c.Get("3"))
+
+	fmt.Println("\nset new value 2 with new lifetime 6 second")
+	c.Set("2", "Ninja", 6)
+	fmt.Println(c.Get("1"), "lifetime 5 sec.")
+	fmt.Println(c.Get("2"), "lifetime 5+6 sec.")
+	fmt.Println(c.Get("3"), "lifetime 5 sec.")
+
+	time.Sleep(time.Second * 4)
+	fmt.Println("\nfrom 6 second")
+	fmt.Println(c.Get("1"), "lifetime 5 sec.")
+	fmt.Println(c.Get("2"), "lifetime 5+6 sec.")
+	fmt.Println(c.Get("3"), "lifetime 5 sec.")
+
+	time.Sleep(time.Second * 2)
+	fmt.Println("\nfrom 8 second")
+	fmt.Println(c.Get("1"), "lifetime 5 sec.")
+	fmt.Println(c.Get("2"), "lifetime 5+6 sec.")
+	fmt.Println(c.Get("3"), "lifetime 5 sec.")
+
+	time.Sleep(time.Second * 4)
+	fmt.Println("\nfrom 12 second")
+	fmt.Println(c.Get("1"), "lifetime 5 sec.")
+	fmt.Println(c.Get("2"), "lifetime 5+6 sec.")
+	fmt.Println(c.Get("3"), "lifetime 5 sec.")
+
+	fmt.Println("\nEnd program")
+}
--- cache.go
+++ cache.go
@@ -1,31 +1,65 @@
 package domz2_1
 
-import "sync"
+import (
+	"sync"
+	"time"
+)
 
-type Cache struct {
-	cacheMap map[string]interface{}
-	mu       sync.Mutex
+type data struct {
+	value    interface{}
+	lifetime int64
 }
 
-func NewCache() Cache {
-	return Cache{cacheMap: map[string]interface{}{}}
+type Cache struct {
+	c  map[string]*data
+	mu sync.Mutex
+}
+
+// New function create new cache and start goroutine for check
+// lifetime of the data
+func New() *Cache {
+	res := &Cache{c: make(map[string]*data)}
+	go res.checkTime()
+	return res
 }
 
 // Set function implementation add data in cache
-func (m *Cache) Set(key string, value interface{}) {
+func (m *Cache) Set(key string, val interface{}, ttl int) {
 	m.mu.Lock()
-	m.cacheMap[key] = value
+	it := &data{value: val}
+	m.c[key] = it
+	m.c[key].lifetime =
+		time.Now().Add(time.Second * time.Duration(ttl)).Unix()
 	m.mu.Unlock()
 }
 
 // Get function implementation get data from cache
 func (m *Cache) Get(key string) interface{} {
-	return m.cacheMap[key]
+	val, ok := m.c[key]
+	if !ok {
+		return nil
+	}
+	return val.value
 }
 
 // Delete function implementation delete data from cache
 func (m *Cache) Delete(key string) {
 	m.mu.Lock()
-	delete(m.cacheMap, key)
+	_, ok := m.c[key]
+	if ok {
+		delete(m.c, key)
+	}
 	m.mu.Unlock()
+}
+
+// checkTime function checks the lifetime of the data
+func (m *Cache) checkTime() {
+	for {
+		time.Sleep(time.Millisecond * 500)
+		for k, v := range m.c {
+			if v.lifetime < time.Now().Unix() {
+				m.Delete(k)
+			}
+		}
+	}
 }
